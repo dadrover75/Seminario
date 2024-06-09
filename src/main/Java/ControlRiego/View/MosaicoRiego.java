c0e5fc6574c7bccead387b16133aa48c3cffac3d
John DoeDiana <dadrover75@hotmail.com>
2024-06-09 18:02:49 -0300

    nuevo comienzo ventanas

--- src/main/Java/ControlRiego/View/MosaicoRiego.java
+++ src/main/Java/ControlRiego/View/MosaicoRiego.java
@@ -1,4 +1,12 @@
 package ControlRiego.View;
 
-public class MosaicoRiego {
+import ControlRiego.Controller.ControlRiegoControl;
+
+import javax.swing.*;
+
+public class MosaicoRiego extends JFrame {
+    private JList list1;
+    private JRadioButton radioButton1;
+    private ControlRiegoControl controlRiegoControl;
+
 }
