8a86315babf4164caecf14de3d48b34a54465406
John DoeDiana <dadrover75@hotmail.com>
2024-06-30 00:26:10 -0300

    entrega 4

---
+++ src/main/Java/Comunication/ConnMQTT/MqttAlternative.java
@@ -0,0 +1,23 @@
+package Comunication.ConnMQTT;
+
+public class MqttAlternative implements IMqttConnection{
+    @Override
+    public void connect() {
+
+    }
+
+    @Override
+    public void disconnect() {
+
+    }
+
+    @Override
+    public void publish(String topic, String message) {
+
+    }
+
+    @Override
+    public void subscribe(String topic) {
+
+    }
+}
